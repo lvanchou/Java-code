3a0cb59f7222bb1e051ffeff33e78a4bc923347f
lvanchou <2418583902@qq.com>
2022-07-30 15:19:25 +0800

    停车场

---
+++ logic/function/src/park.java
@@ -0,0 +1,14 @@
+public class park {
+    public static void main(String[] args) {
+        int num = 1250;
+      for(int x=1;x<=5;x++)
+        System.out.println("ʣ�೵λ"+park1(num));
+    }
+    public static int num=1250;
+    public static int park1(int num1){
+        num--;
+        num1=num;
+    return num1;
+        }
+    }
+
